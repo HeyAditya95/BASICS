4ca2ff8e6e2e64fd98661eff0fff984eb8029f05
HeyAditya95 <haaditya95b@gmail.com>
2024-02-29 11:51:01 +0530

    meta characters and matching symbol of regular expression

--- Regular_expressions.java
+++ Regular_expressions.java
@@ -4,7 +4,11 @@ public class Regular_expressions {
         String str2 = "k"; //can be anything h8 ,G5 ,h6 , f5 etc. there can be 
         String str3 = "g2";
         String str4 = "b2B";
+        String str5 = "3";
+        String str6 = " ";
+    
 
+        //MATCHING SYMBOLS
 
         System.out.println(str1.matches("."));   // "." can be compare to any digit and letter or symbols    --true
 
@@ -13,15 +17,25 @@ public class Regular_expressions {
         // and for that [] big bracket is important    --false
 
         System.out.println(str2.matches("[a-z0-9]"));   // this is method sets the range of alphabet from your desired alphabet and from your desired number   --true
-        //but this will only compare letter and numbers not special characters or symbol
+        //but this will only compare letter and numbers not special characters or symbol    --true
 
         System.out.println(str3.matches("[a-z][0-9]"));  //for comparing two characters we have to use two separates big bracket [][]
-
+        //  --true
         System.out.println(str4.matches("[a-z][A-Z][0-9]")); //for comparing three characters , we have to use three separates big bracket [][][]
-
+        // --false
         System.out.println(str1.matches("[^abc]"));  // ^ is known as except , for eg if we have [^abc] ,this means string should contain all letters except abc
+        // --true
+        System.out.println(str2.matches("a|k"));   // "|" this means must be     --
+        
+        //META CHARACTERS
 
-        System.out.println(str2.matches("a|k"));   // "|" this means must be     --true
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------");
+
+        System.out.println(str1.matches("\\s"));   //     --false
+        System.out.println(str2.matches("\\w"));   //    --true
+        System.out.println(str5.matches("\\D"));   //    --false
+        System.out.println(str6.matches("\\s"));   //     --true
+
 
     }
     
